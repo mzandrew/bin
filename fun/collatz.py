72c45b30635b367f582285b6138cdde541480b82
mzandrew <mzandrew@gmail.com>
2016-08-30 23:27:54 -0200

    first version to compute results of collatz conjecture depth calculations and store them in a sqlite3 database

--- fun/collatz.py
+++ fun/collatz.py
@@ -22,7 +22,7 @@ def start(input):
     else:
         start(odd(input))
 
-def colatz(number):
+def collatz(number):
     global depth
     depth = 0
     start(number)
@@ -31,50 +31,50 @@ def colatz(number):
     return list
 
 def count(number):
-    cursor.execute("select count(*) from colatz where number=?", (number,))
+    cursor.execute("select count(*) from collatz where number=?", (number,))
     count = cursor.fetchone()[0]
     return count
 
 def insert(number, depth):
     if count(number) == 0:
         #print "adding " + str(number)
-        cursor.execute("insert into colatz(number, depth) values(?, ?)", (number, depth))
+        cursor.execute("insert into collatz(number, depth) values(?, ?)", (number, depth))
     else:
         #print "updating"
-        cursor.execute("update colatz set depth=? where number=?", (depth, number))
+        cursor.execute("update collatz set depth=? where number=?", (depth, number))
 
-connection = sqlite3.connect("colatz.sqlite")
+connection = sqlite3.connect("collatz.sqlite")
 with connection:
     cursor = connection.cursor()
     #cursor.execute("select name from sqlite_master where type='table'")
     cursor.execute('select name from sqlite_master where type=?', ('table',))
-    found_colatz_table = 0
+    found_collatz_table = 0
     rows = cursor.fetchall()
     for row in rows:
-        if row[0]=="colatz":
+        if row[0]=="collatz":
             print "found table " + row[0]
-            found_colatz_table = 1
-    if not found_colatz_table:
-        #cursor.execute("create table colatz(id INT, number INT, depth INT)")
-        cursor.execute("create table colatz(id integer primary key, number INT, depth INT)")
-    #cursor.execute("select table colatz")
+            found_collatz_table = 1
+    if not found_collatz_table:
+        #cursor.execute("create table collatz(id INT, number INT, depth INT)")
+        cursor.execute("create table collatz(id integer primary key, number INT, depth INT)")
+    #cursor.execute("select table collatz")
     #last_row_id = cursor.lastrowid
     #if last_row_id == None:
     #    print "nothing in table"
     #else:
     #    print "last id#" + str(last_row_id)
-    #cursor.execute("insert into colatz values(?, ?, ?)", (1, 7, 0))
-    #cursor.execute("insert into colatz(number, depth) values(?, ?)", (7, 0))
+    #cursor.execute("insert into collatz values(?, ?, ?)", (1, 7, 0))
+    #cursor.execute("insert into collatz(number, depth) values(?, ?)", (7, 0))
     for i in range(1, 20):
         if count(i) == 0:
-            (number, depth) = colatz(i)
+            (number, depth) = collatz(i)
             insert(number, depth)
  #   last_row_id = cursor.lastrowid
  #   if last_row_id == None:
  #       print "nothing in table"
  #   else:
  #       print "last id#" + str(last_row_id)
-    cursor.execute("select * from colatz")
+    cursor.execute("select * from collatz")
     rows = cursor.fetchall()
     if rows == None:
         print "nothing in table"
