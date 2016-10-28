dd8c672eae994aed22efa88acbc2cf7dfea4872d
mza <mzandrew@gmail.com>
2016-10-28 09:36:47 -1000

    makes a tar file with everything that svn status reports as M (modified) and another tarfile for the things marked ? (unknown)

---
+++ repo/tar_up_everything_that_svn_status_says_is_modified.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+
+declare tarfilebase="$1"
+if [ -z "$tarfilebase" ]; then
+	echo "ERROR:  must specify a base filename for the tarfiles on the command line"
+	exit 1
+fi
+
+declare -i count
+declare tarfile
+
+tarfile="$tarfilebase.modified.tar"
+count=0
+if [ -e "$tarfile" ]; then
+	echo "ERROR:  file \"$tarfile\" already exists"
+	exit 2
+fi
+svn status | grep ^M | sed -e "s,^M       ,," | while read file; do
+	#echo "$file"
+	ls -lart $file
+	if [ $count -eq 0 ]; then
+		tar cf "$tarfile" $file
+	else
+		tar rf "$tarfile" $file
+	fi
+	count=$((count+1))
+done
+oldtarfile="$tarfile"
+
+tarfile="$tarfilebase.question-mark.tar"
+count=0
+if [ -e "$tarfile" ]; then
+	echo "ERROR:  file \"$tarfile\" already exists"
+	exit 2
+fi
+svn status | grep ^? | sed -e "s,^?       ,," | while read file; do
+	#echo "$file"
+	if [ "$file" = "$oldtarfile" ]; then
+		continue
+	fi
+	ls -lart $file
+	if [ $count -eq 0 ]; then
+		tar cf "$tarfile" $file
+	else
+		tar rf "$tarfile" $file
+	fi
+	count=$((count+1))
+done
+
