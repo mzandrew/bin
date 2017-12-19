2b4d08679b109014fb9b04e1aefccf33bdd2aa3e
mzandrew <mzandrew@gmail.com>
2017-12-19 00:42:51 -1000

    moved git config stuff to as_you_like_it; added preliminary version of that for archlinux

---
+++ setup/as_you_like_it.archlinux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+cd
+mkdir -p build # for development
+
+cd
+cd build
+git clone https://github.com/mzandrew/bin.git
+
+cd
+mkdir -p bin
+cd bin
+ln -s ../build/bin/generic
+
+cd
+cat >> .bashrc <<HERE
+if [ -e $HOME/build/bin/nofizbin/bashrc ]; then
+	. $HOME/build/bin/nofizbin/bashrc
+fi
+HERE
+
+cd
+mkdir -p tmp # for vim swap files
+
+cd
+ln -s build/bin/nofizbin/.vimrc
+
+which git 2>&1 > /dev/null
+if [ $? -eq 0 ]; then
+	git config --global user.name mzandrew
+	echo "git config --global user.email blah@org"
+	#git config --global push.default simple
+fi
+
