124840289515d24a868bc679be60d96523f965cb
mzandrew <mza@phys.hawaii.edu>
2018-07-20 07:18:47 -1000

    bugfix

--- generic/arping_everyone_quickly.sh
+++ generic/arping_everyone_quickly.sh
@@ -3,11 +3,12 @@
 #for i in $(seq 2 253); do sleep 1; ping -c1 -w1 192.168.10.$i >/dev/null; echo "$i $?"; done
 
 declare baseip="192.168.10"
+declare device="eth6"
 
 function go {
 	local ip="$1"
 	#echo "ip=$1"
-	arping -c1 -w1 $ip >/dev/null && echo > "$dir/$ip" || /bin/true
+	arping -c1 -w1 $ip -I$device >/dev/null && echo > "$dir/$ip" || /bin/true
 }
 
 declare dir=$(mktemp -d)
