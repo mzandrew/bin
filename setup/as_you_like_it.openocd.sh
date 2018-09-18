e1705c6da43c2bc9c21cce66ffc8af53b3569571
mza <mzandrew@gmail.com>
2018-09-18 13:29:48 -1000

    dependencies and git clone/build for openocd

---
+++ setup/as_you_like_it.openocd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+function install_packages {
+	nice sudo apt -y install libtool autoconf automake libusb-1.0 libtcl8.5 tcl8.5 make gcc clang pkg-config texinfo
+}
+
+install_packages
+
+cd
+mkdir -p build
+
+if [ ! -e ~/build/openocd ];then
+	cd ~/build
+	git clone git://git.code.sf.net/p/openocd/code openocd
+	cd openocd
+	nice ./bootstrap.sh
+fi
+
+cd ~/build/openocd
+nice ./configure --enable-ftdi --enable-buspirate
+nice make
+sudo make install
+
