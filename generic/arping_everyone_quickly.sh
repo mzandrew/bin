824aaa1e8ac43b3192a716bd629fb8268132f81f
mzandrew <mza@phys.hawaii.edu>
2018-12-11 15:41:18 -1000

    take interface as argument; don't arping self

--- generic/arping_everyone_quickly.sh
+++ generic/arping_everyone_quickly.sh
@@ -3,12 +3,15 @@
 #for i in $(seq 2 253); do sleep 1; ping -c1 -w1 192.168.10.$i >/dev/null; echo "$i $?"; done
 
 declare baseip="192.168.10"
-declare device="eth6"
+declare device="${1:-eth6}"
+declare self=$(ifconfig $device | grep 'inet addr' | sed -e "s,[ ]\+inet addr:$baseip.\([0-9]\+\) .*,\1,")
 
 function go {
 	local ip="$1"
 	#echo "ip=$1"
-	arping -c1 -w1 $ip -I$device >/dev/null && echo > "$dir/$ip" || /bin/true
+	if [ ! "$ip" == "$baseip.$self" ]; then
+		arping -c1 -w1 $ip -I$device >/dev/null && echo > "$dir/$ip" || /bin/true
+	fi
 }
 
 declare dir=$(mktemp -d)
