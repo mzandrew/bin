23f6d5e70ee18c7b56dca001aaf610e64af0afa7
mza <mzandrew@gmail.com>
2020-05-05 11:03:42 -1000

    adding one more

--- setup/as_you_like_it.raspbian.sh
+++ setup/as_you_like_it.raspbian.sh
@@ -26,7 +26,7 @@ function install_packages_1 {
 	sudo apt -y install mlocate git subversion rsync ntp
 	sudo apt -y update
 	sudo apt -y upgrade
-	sudo apt -y install firefox-esr nfs-common nfs-tools tmux
+	sudo apt -y install firefox-esr nfs-common tmux
 	sudo apt -y install network-manager-openconnect-gnome # for VPN
 	sudo apt -y install libcanberra-gtk-module libcanberra-gtk3-module # to avoid annoying messages
 	sudo apt -y install vim-gtk cmake
@@ -35,7 +35,7 @@ function install_packages_1 {
 }
 
 function install_packages_2 {
-	sudo apt install raspinfo rpi.gpio-common raspi-gpio python-rpi.gpio python3-rpi.gpio python3-gpiozero python-gpiozero python3-spidev python-spidev i2c-tools spi-tools python3-picamera python-picamera u-boot-rpi rpiboot lm-sensors lshw # rpi-specific
+	sudo apt install raspinfo rpi.gpio-common raspi-gpio python-rpi.gpio python3-rpi.gpio python3-gpiozero python-gpiozero python3-spidev python-spidev i2c-tools spi-tools python3-picamera python-picamera u-boot-rpi rpiboot lm-sensors lshw wiringpi # rpi-specific
 	# rpi-chromium-mods # includes flash player
 	#sudo apt install mpd mpc easytag audacious # audio stuff
 	# python3-pychrome mkchromecast castpulseaudio-dlna
