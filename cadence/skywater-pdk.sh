c8ae4cc4d5c9a74e0fc730297985437aae6b80a7
mzandrew <mzandrew@gmail.com>
2020-07-08 07:52:55 -1000

    adding git clone script to grab all of the skywater pdk

---
+++ cadence/skywater-pdk.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash -e
+
+git clone https://github.com/google/skywater-pdk
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_hd
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_hdll
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_hs
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_hvl
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_lp
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_ls
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_fd_sc_ms
+
