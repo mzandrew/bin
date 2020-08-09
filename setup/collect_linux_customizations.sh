56615352af382227eb125396c6b2a2c4856718de
mzandrew <mza@phys.hawaii.edu>
2020-08-09 08:42:33 -1000

    modified to work for centos too

--- setup/collect_linux_customizations.sh
+++ setup/collect_linux_customizations.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# last updated 2020-08-09 by mza
+
 declare date=$(date +"%Y-%m-%d")
 declare hostname=$(hostname)
 mkdir -p ${HOME}/build
@@ -13,10 +15,24 @@ list="$list etc/shadow"
 list="$list etc/fstab"
 list="$list etc/sysctl.conf"
 list="$list etc/default/keyboard"
+list="$list etc/sysconfig/keyboard"
 list="$list etc/ntp.conf"
 list="$list etc/apt/apt.conf"
+list="$list etc/yum.conf"
+list="$list etc/yum.repos.d"
 list="$list boot/config.txt"
 #list="$list boot/SSH"
+
+declare final_list=""
 cd /
-sudo tar cf ${HOME}/build/${date}.${hostname}.raspbian-customizations.tar $list
+for each in $list; do
+	if [ -e "$each" ]; then
+		final_list="$final_list $each"
+#	else
+#		echo "file \"$each\" not found"
+	fi
+done
+
+mkdir -p "${HOME}/build"
+sudo tar cf "${HOME}/build/${date}.${hostname}.linux-customizations.tar" $final_list
 
