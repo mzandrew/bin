e513f85de45cc8ceb4728ecb3b19985dfebab803
mza <mzandrew@gmail.com>
2020-11-04 08:32:43 -1000

    quick script to do the things I always do after buring a raspios/raspbian image

---
+++ setup/customize_raspios_after_image_burn.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+# written 2020-10-28 by mza
+# last updated 2020-11-04 by mza
+
+declare hostname="pi#7-rpi0w"
+declare device="/dev/sdd"
+declare wpa_supplicant="/root/wpa_supplicant.conf"
+
+sudo mkdir -p /media/boot
+sudo mount ${device}1 /media/boot
+
+sudo mkdir -p /media/root
+sudo mount ${device}2 /media/root
+
+# enable ssh (warning: change default password for user "pi"):
+sudo touch /media/boot/SSH
+
+sudo mkdir /media/root/root/build
+#sudo git clone https://github.com/mzandrew/eink-clock.git /media/root/root/build/eink-clock
+
+#function rc_local_insert {
+	#sudo sed -ie '/'$1'/{h;s/.*/'$2'/};${x;/^$/{s/.*/'$2'/;H};x}' /media/root/etc/rc.local
+#	sudo sed -ie 's/^exit 0$//;' /media/root/etc/rc.local
+#}
+#rc_local_insert 'eink-clock' 'nice /root/build/eink-clock/clock.py \&'
+#nice /root/build/led-matrix-clock/clock.py &
+
+sudo cp -a $wpa_supplicant /media/root/etc/wpa_supplicant/wpa_supplicant.conf || /bin/true
+
+echo "$hostname" | sudo tee /media/root/etc/hostname
+
+sudo umount /media/boot
+sudo umount /media/root
+
