204723a08bd34f578314fe10f94940845f40bb02
mza <mzandrew@gmail.com>
2020-11-25 06:20:58 -1000

    made them all work together

--- embedded/ina260_adafruit.py
+++ embedded/ina260_adafruit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # written 2020-11-25 by mza
+# last updated 2020-11-25 by mza
 
 # from https://learn.adafruit.com/adafruit-ina260-current-voltage-power-sensor-breakout/python-circuitpython
 # and https://github.com/mzandrew/XRM/blob/master/predator/IV.py
@@ -11,23 +12,52 @@ import board
 import busio
 import adafruit_ina260 # sudo pip3 install adafruit-circuitpython-ina260
 
-i2c = busio.I2C(board.SCL, board.SDA)
-ina260 = adafruit_ina260.INA260(i2c_bus=i2c, address=0x40)
+def setup(address=0x40):
+	i2c = busio.I2C(board.SCL, board.SDA)
+	global ina260
+	ina260 = adafruit_ina260.INA260(i2c_bus=i2c, address=address)
+
+header_string = ", current (mA), voltage (V), power (mW)"
 
 def print_header():
-	print("#time, current (mA), voltage (V), power (mW)")
+	print("#time" + header_string)
 
-def print_compact():
-	date = time.strftime("%Y-%m-%d+%X")
+def measure():
 	I = ina260.current
 	V = ina260.voltage
 	P = ina260.power
-	print("%s, %.1f, %.3f, %.1f" % (date, I, V, P))
+	return I, V, P
 
-print_header()
-while True:
-	#print_verbose()
-	print_compact()
-	sys.stdout.flush()
-	time.sleep(1)
+def measure_string():
+	I, V, P = measure()
+	return "%.1f, %.3f, %.1f" % (I, V, P)
+
+def print_compact():
+	date = time.strftime("%Y-%m-%d+%X")
+	string = measure_string()
+	print("%s, %s" % (date, string))
+
+def test_if_present():
+	try:
+		ina260.current
+	except:
+		return False
+	return True
+
+address = 0x40
+
+def hex(number, width=1):
+	return "%0*x" % (width, number)
+
+if __name__ == "__main__":
+	try:
+		setup(address)
+	except:
+		print("ina260 not present at address 0x" + hex(address))
+		sys.exit(1)
+	print_header()
+	while test_if_present():
+		print_compact()
+		sys.stdout.flush()
+		time.sleep(1)
 
--- embedded/pct2075_adafruit.py
+++ embedded/pct2075_adafruit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # written 2020-11-24 by mza
+# last updated 2020-11-25 by mza
 
 # from https://learn.adafruit.com/adafruit-pct2075-temperature-sensor/python-circuitpython
 
@@ -10,25 +11,44 @@ import board
 import busio
 import adafruit_pct2075
 
-i2c = busio.I2C(board.SCL, board.SDA)
-pct = adafruit_pct2075.PCT2075(i2c)
+def setup():
+	i2c = busio.I2C(board.SCL, board.SDA)
+	global pct
+	pct = adafruit_pct2075.PCT2075(i2c)
 
-#def once():
-#	temp = pct.temperature
-#	string = "%.1f C" % temp
-#	print(string)
-#once
+header_string = ", temperature (C)"
 
 def print_header():
-	print("#time, temperature (C)")
+	print("#time" + header_string)
+
+def measure():
+	return pct.temperature
+
+def measure_string():
+	temp = measure()
+	return "%.1f" % temp
 
 def print_compact():
-	print("%s, %0.1f" % (time.strftime("%Y-%m-%d+%X"),pct.temperature))
+	date = time.strftime("%Y-%m-%d+%X")
+	string = measure_string()
+	print("%s, %s" % (date, string))
 
-print_header()
-while True:
-	#print_verbose()
-	print_compact()
-	sys.stdout.flush()
-	time.sleep(1)
+def test_if_present():
+	try:
+		pct.temperature
+	except:
+		return False
+	return True
+
+if __name__ == "__main__":
+	try:
+		setup()
+	except:
+		print("pct2075 not present")
+		sys.exit(1)
+	print_header()
+	while test_if_present():
+		print_compact()
+		sys.stdout.flush()
+		time.sleep(1)
 
