eaed78c8150cd01cd9a374f7ba8c390bc13a850a
mza <mzandrew@gmail.com>
2020-12-11 19:01:23 +0000

    can specify range now

--- generic/ping_everyone_quickly.sh
+++ generic/ping_everyone_quickly.sh
@@ -2,7 +2,12 @@
 
 #for i in $(seq 2 253); do sleep 1; ping -c1 -w1 192.168.10.$i >/dev/null; echo "$i $?"; done
 
-declare baseip="192.168.10"
+# last updated 2020-12-11 by mza
+
+#declare baseip="192.168.10"
+declare baseip=$(ifconfig | grep inet | grep 192.168 | awk '{ print $2 }' | sed -e "s,\.[0-9]\+$,,")
+#echo $baseip
+declare dir=$(mktemp -d)
 
 function go {
 	local ip="$1"
@@ -10,8 +15,14 @@ function go {
 	ping -c1 -w1 $ip >/dev/null && echo > "$dir/$ip" || /bin/true
 }
 
-declare dir=$(mktemp -d)
-for i in $(seq 1 254); do
+declare -i first=1
+declare -i last=254
+if [ $# -gt 1 ]; then
+	first=$1
+	last=$2
+fi
+#echo "$first $last"
+for i in $(seq $first $last); do
 	go "$baseip.$i" &
 done
 sleep 2
