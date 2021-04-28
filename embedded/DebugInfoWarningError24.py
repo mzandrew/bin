77bc601ea4f7aaa73e11ffcafd4ee78c869bb5e4
mza <mzandrew@gmail.com>
2021-04-28 06:25:56 -1000

    bugfixes; attempts to communicate with any and all pct2075 i2c addresses

--- embedded/DebugInfoWarningError24.py
+++ embedded/DebugInfoWarningError24.py
@@ -1,6 +1,6 @@
 # written 2019-01-07 by mza to support python2.4-era SL5.11 on COPPERs
 # updated 2019-03-08 to write to logfiles
-# last updated 2020-12-18 by mza
+# last updated 2021-04-28 by mza
 
 import sys # stderr.write()
 import time # strftime
@@ -21,34 +21,64 @@ def set_verbosity(value):
 	return original_verbosity
 
 def create_new_logfile_with_string(string):
-	global logfile
 	global logfile_is_open
-	logdirname = "logs"
-	#timestring = time.strftime("%Y-%m-%d.%H:%M:%S")
-	timestring = time.strftime("%Y-%m-%d.%H%M%S")
-	logfilename = logdirname + "/" + timestring + "." + string + ".log"
-	if not os.path.isdir(logdirname):
-		info("creating dir \"" + logdirname + "\"...")
-		os.mkdir(logdirname)
-	if os.path.isfile(logfilename):
-		error("ERROR opening logfile %s (file already exists), exiting" % logfilename)
-		sys.exit(1)
+	logfile_is_open = 0
+	global logfile
 	try:
-		logfile = open(logfilename, "w")
+		logdirname = "logs"
+		try:
+			os.mkdir(logdirname)
+			info("created dir \"" + logdirname + "\"")
+		except:
+			pass
+		try:
+			timestring = time.strftime("%Y-%m-%d.%H%M%S")
+			logfilename = logdirname + "/" + timestring + "." + string + ".log"
+		except:
+			logfilename = logdirname + "/" + string + ".log"
+		#info("logfilename = " + logfilename)
+#		if os.path.isfile(logfilename):
+#			error("ERROR opening logfile %s (file already exists), exiting" % logfilename)
+#			sys.exit(1)
+		try:
+			logfile = open(logfilename, "a")
+			#info("logfilename = " + logfilename)
+		except:
+			try:
+				logfilename = string + ".log"
+				#info("logfilename = " + logfilename)
+				logfile = open(logfilename, "a")
+			except:
+				pass
+		try:
+			atexit.register(cleanup)
+		except:
+			pass
+		try:
+			info("Writing output from %s to logfile: %s" % (sys.argv[0], logfilename))
+		except:
+			pass
+		logfile_is_open = 1
 	except:
-		error("ERROR opening logfile %s, exiting" % logfilename)
-		sys.exit(2)
-	logfile_is_open = 1
+		error("Unable to open logfile")
+
+def create_new_logfile_with_string_embedded(string):
+	global logfile_is_open
+	logfile_is_open = 0
+	global logfile
 	try:
-		atexit.register(cleanup)
+		logfile = open(string + ".log", "a")
+		logfile_is_open = 1
 	except:
-		pass
-	info("Writing output from %s to logfile: %s" % (sys.argv[0], logfilename))
+		error("Unable to open logfile")
 
 def print_string_stdout(string, should_flush=1):
 	print(string)
 	if should_flush:
-		sys.stdout.flush()
+		try:
+			sys.stdout.flush()
+		except:
+			pass
 
 def print_string_stderr(string, should_flush=1):
 	sys.stderr.write(string + "\n")
@@ -59,36 +89,38 @@ def print_string_stderr(string, should_flush=1):
 			pass
 
 def print_string_logfile(string, should_flush=1):
-	if (logfile_is_open):
+	if logfile_is_open:
 		logfile.write(string + "\n")
+		logfile.flush()
+		#print("wrote to logfile")
 		if should_flush:
 			logfile.flush()
 
 def debug3(string, should_flush=1):
-	if (verbosity>=6):
+	if verbosity>=6:
 		string = "  DEBUG3:  " + string
 		print_string_stdout(string, should_flush)
 		print_string_logfile(string, should_flush)
 
 def debug2(string, should_flush=1):
-	if (verbosity>=5):
+	if verbosity>=5:
 		string = "  DEBUG2:  " + string
 		print_string_stdout(string, should_flush)
 		print_string_logfile(string, should_flush)
 
 def debug(string, should_flush=1):
-	if (verbosity>=4):
+	if verbosity>=4:
 		string = "   DEBUG:  " + string
 		print_string_stdout(string, should_flush)
 		print_string_logfile(string, should_flush)
 
 def info(string, should_flush=1):
-	if (verbosity>=3):
+	if verbosity>=3:
 		print_string_stdout(string, should_flush)
 		print_string_logfile(string, should_flush)
 
 def warning(string, should_flush=1):
-	if (verbosity>=2):
+	if verbosity>=2:
 		string = " WARNING:  " + string
 		print_string_stderr(string, should_flush)
 		print_string_logfile(string, should_flush)
--- embedded/temperature_log_and_graph.py
+++ embedded/temperature_log_and_graph.py
@@ -1,34 +1,60 @@
 #!/usr/bin/env python3
 
 # written 2021-04-21 by mza
-# last updated 2021-04-2by mza
+# last updated 2021-04-28 by mza
 
 import time
 import sys
 import board
 import busio
 import displayio
+import neopixel
 import adafruit_pct2075 # sudo pip3 install adafruit-circuitpython-pct2075
 import adafruit_ssd1327 # sudo pip3 install adafruit-circuitpython-ssd1327
-from DebugInfoWarningError24 import debug, info, warning, error, debug2, debug3, set_verbosity, create_new_logfile_with_string
+from DebugInfoWarningError24 import debug, info, warning, error, debug2, debug3, set_verbosity, create_new_logfile_with_string_embedded
+
+temperature_sensors = []
 
 def setup_temperature_sensor(address):
 	#i2c.deinit()
-	global pct
-	pct = adafruit_pct2075.PCT2075(i2c, address=address)
+	global temperature_sensors
+	temperature_sensors.append(adafruit_pct2075.PCT2075(i2c, address=address))
+	return 1
 
-displayio.release_displays()
+def setup_temperature_sensors():
+	count = 0
+	for address in [0x37, 0x36, 0x35, 0x2, 0x2e, 0x2d, 0x2c, 0x2b, 0x2a, 0x29, 0x28, 0x77, 0x76, 0x75, 0x74, 0x73, 0x72, 0x71, 0x70, 0x4f, 0x4e, 0x4d, 0x4c, 0x4b, 0x4a, 0x49, 0x48]:
+		try:
+			count += setup_temperature_sensor(address)
+		except:
+			pass
+	if 0==count:
+		error("pct2075 not present (any i2c address)")
+	else:
+		info("found " + str(count) + " temperature sensors")
+	return count
+
 header_string = "temperature (C)"
 
 def print_header():
 	info("#" + header_string)
 
 def measure():
-	return pct.temperature
+	result = []
+	for each in temperature_sensors:
+		try:
+			result.append(each.temperature)
+		except:
+			pass
+	return result
 
 def measure_string():
-	temp = measure()
-	return "%.1f" % temp
+	result = measure()
+	#string = ", ".join(result)
+	string = "%.1f" % result.pop(0)
+	for each in result:
+		string += ", %.1f" % each
+	return string
 
 def print_compact():
 	try:
@@ -40,7 +66,7 @@ def print_compact():
 
 def test_if_present():
 	try:
-		pct.temperature
+		temperature_sensors[0].temperature
 	except:
 		return False
 	return True
@@ -50,26 +76,42 @@ def setup_i2c_oled_display(address):
 	global display
 	display = adafruit_ssd1327.SSD1327(display_bus, width=128, height=128)
 
+def setup_neopixel():
+	global pixel
+	pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.01, auto_write=True)
+
 if __name__ == "__main__":
+	try:
+		displayio.release_displays()
+	except:
+		pass
+	try:
+		setup_neopixel()
+	except:
+		pass
 	#info("start")
-	i2c = busio.I2C(board.SCL1, board.SDA1)
+	try:
+		i2c = busio.I2C(board.SCL1, board.SDA1)
+	except:
+		i2c = busio.I2C(board.SCL, board.SDA)
 	#info("temperature sensor")
 	try:
-		setup_temperature_sensor(0x37)
+		setup_temperature_sensors()
 	except:
-		error("pct2075 not present (address 0x37)")
-		sys.exit(1)
+		error("can't find any temperature sensors on i2c bus")
 	#info("display")
 	try:
 		setup_i2c_oled_display(0x3d)
 	except:
 		error("can't initialize ssd1327 display over i2c (address 0x3d)")
-		sys.exit(1)
+		#sys.exit(1)
 	#info("continue")
-	create_new_logfile_with_string("pct2075")
+	create_new_logfile_with_string_embedded("pct2075")
 	print_header()
 	while test_if_present():
+		pixel.fill((255, 0, 0))
 		print_compact()
+		pixel.fill((0, 255, 0))
 		try:
 			sys.stdout.flush()
 		except:
