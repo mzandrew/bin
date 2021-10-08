395b53d59c7fa1589fab4ce228c9237ee3ae3b57
mza <mza@phys.hawaii.edu>
2021-10-07 15:17:36 -1000

    added cleanup code

--- embedded/pibuttons.py
+++ embedded/pibuttons.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
 
 # written 2021-10-06 by mza
-# last updated 2021-10-06 by mza
+# last updated 2021-10-07 by mza
 # for use with pibuttons PCB https://oshpark.com/shared_projects/DUR2zMFP
 # with help from https://raspi.tv/2013/rpi-gpio-basics-6-using-inputs-and-outputs-together-with-rpi-gpio-pull-ups-and-pull-downs
 
 import time
+import atexit
+import subprocess
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+ios = []
+
+def cleanup():
+#	print("cleanup")
+#	for io in ios:
+#		func = GPIO.gpio_function(io)
+#		print(str(io) + " " + str(func))
+	GPIO.cleanup()
+	GPIO.setmode(GPIO.BCM)
+#	for io in ios:
+#		#GPIO.setup(io, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
+#		func = GPIO.gpio_function(io)
+#		print(str(io) + " " + str(func))
+	command = ["gpio", "-g", "mode", "2", "ALT0"]
+	subprocess.run(command)
+	command = ["gpio", "-g", "mode", "3", "ALT0"]
+	subprocess.run(command)
+#	for io in ios:
+#		func = GPIO.gpio_function(io)
+#		print(str(io) + " " + str(func))
 
 def setup(buttons):
+	global ios
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
 	for b in buttons:
 #		print(str(b))
 #		try:
@@ -20,6 +44,8 @@ def setup(buttons):
 			GPIO.setup(b, GPIO.IN)
 		else:
 			GPIO.setup(b, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	ios = buttons
+	atexit.register(cleanup)
 
 def show_buttons():
 	for b in buttons:
