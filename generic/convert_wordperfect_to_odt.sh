4b865098946b70854269a9a5a71712a93b902c88
mza <mzandrew@gmail.com>
2022-04-06 11:37:48 -1000

    rewrote it so it works well recursively; original files go in tarfile

--- generic/convert_wordperfect_to_odt.sh
+++ generic/convert_wordperfect_to_odt.sh
@@ -1,30 +1,68 @@
-#!/bin/bash -e
-
-# sudo apt install -y wpd2odt
+#!/bin/bash
 
 # written 2022-04-03 by mza
+# last updated 2022-04-06 by mza
+
+which wpd2odt >/dev/null
+if [ $? -ne 0 ]; then
+	echo "wpd2odt required.  try:\nsudo apt install -y wpd2odt"
+	exit
+fi
+
+declare originals_tar="originals.tar"
+
+function run_on_file {
+	local each="${1}"
+	#echo "found file \"${each}\""
+	#if [ "${each}" == "." ]; then continue; fi
+	#if [ "${each}" == ".." ]; then continue; fi
+	if [ "${each: -4}" == ".odt" ]; then return; fi
+	if [ "${each: -4}" == ".tar" ]; then return; fi
+	local is_wordperfect_file=0
+	local new="${each}.odt"
+	if [ ! -e "${each}.odt" ]; then
+		is_wordperfect_file=$(file "${each}" | grep -ci wordperfect)
+		if [ $is_wordperfect_file -gt 0 ]; then
+			echo "${each}"
+			wpd2odt "${each}" "${new}"
+			if [ $? -ne 0 ]; then
+				rm -f "${new}"
+				file "${each}"
+			else
+				touch --reference="$each" "${new}"
+				(
+					local dirname="$(dirname "${each}")"
+					local basename="$(basename "${each}")"
+					#echo "$dirname $basename"
+					cd "$dirname"
+					tar rf "${originals_tar}" "${basename}" --remove-files
+				)
+			fi
+		fi
+	fi
+}
+
+function run_on_dir {
+	local each="${1}"
+	#echo "found dir \"${each}\""
+	find "$each" -type f -exec $0 "{}" +
+}
 
 if [ $# -gt 0 ] ;then
 	for each; do
-		#if [ "${each}" == "." ]; then continue; fi
-		#if [ "${each}" == ".." ]; then continue; fi
-		if [ "${each: -3}" == "odt" ]; then continue; fi
 		if [ -e "${each}" ]; then
 			if [ -d "${each}" ]; then
-				#find "$each" -exec $0 "{}" +
-				continue
-			fi
-			#new="$(basename "$each").odt"
-			new="${each}.odt"
-			if [ ! -e "${each}.odt" ]; then
-				echo "$each"
-				wpd2odt "${each}" > "${new}" && touch --reference="$each" "${new}" || rm "${new}" && file "${each}"
+				run_on_dir "${each}"
+			elif [ -L "${each}" ]; then
+				echo "ignoring symlink"
+			else
+				run_on_file "${each}"
 			fi
 		else
-			echo "can't find file $each"
+			echo "can't find \"$each\""
 		fi
 	done
 else
-	find . -exec $0 "{}" +
+	find . -type f -exec $0 "{}" +
 fi
 
