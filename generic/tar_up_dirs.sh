537f290d9b4a1df6dcc546fe349b4d8b6b35a54d
mza <mzandrew@gmail.com>
2022-04-18 07:40:39 -1000

    add .tar.lf-r file before adding files to archive

--- generic/tar_up_dirs.sh
+++ generic/tar_up_dirs.sh
@@ -4,16 +4,22 @@ declare temp_file="$(mktemp /tmp/datestamp.XXXXXX)"
 
 for each in * .*; do
 	declare tar="${each}.tar"
+	declare lfr="${tar}.lf-r"
 	if [ "$each" == ".." ]; then continue; fi
 	if [ "$each" == "." ]; then continue; fi
 	#echo "$each"
 	if [ -d "$each" ]; then
 		if [ ! -e "$tar" ]; then
-			echo $each
+			echo "$each"
+			(
+				cd "$each"
+				lf > "../${lfr}"
+			)
 			touch --reference="$each" "$temp_file"
 			tar cf "$tar" "$each" --remove-files
 			touch --reference="$temp_file" "$tar"
-			ls -lart "$tar"
+			touch --reference="$temp_file" "$lfr"
+			ls -lart "$tar" "$lfr"
 		else
 			echo "file \"${tar}\" already exists"
 		fi
