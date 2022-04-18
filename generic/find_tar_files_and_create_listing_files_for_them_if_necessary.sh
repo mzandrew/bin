c4bd6ff6f152a71d2571f276d2a56d20a30b3218
mza <mzandrew@gmail.com>
2022-04-18 05:00:21 -1000

    minor bugfix

--- generic/find_tar_files_and_create_listing_files_for_them_if_necessary.sh
+++ generic/find_tar_files_and_create_listing_files_for_them_if_necessary.sh
@@ -2,6 +2,6 @@
 
 script="$(dirname $0)/extract-lf-listing-from-tar-file"
 #echo "${script}"
-find "${@}" -type f \( -iname "*tar" -o -iname "*.tar.gz" -o -iname "*.tar.bzip2" \) -exec ${script} {} +
+find "${@}" -type f \( -iname "*.tar" -o -iname "*.tar.gz" -o -iname "*.tar.bzip2" \) -exec ${script} {} +
 #find -iname "*tar" -o -iname "*.tar.gz" -o -iname "*.tar.bzip2"
 
