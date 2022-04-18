eef222e965c705a8275c342f94c9df86e801bc66
mza <mzandrew@gmail.com>
2022-04-18 18:28:45 +0100

    lets tar remove the files

--- generic/tar_up_dirs.sh
+++ generic/tar_up_dirs.sh
@@ -1,13 +1,23 @@
-#!/bin/bash -e
+#!/bin/bash
+
+declare temp_file="$(mktemp /tmp/datestamp.XXXXXX)"
 
 for each in * .*; do
+	declare tar="${each}.tar"
 	if [ "$each" == ".." ]; then continue; fi
 	if [ "$each" == "." ]; then continue; fi
 	#echo "$each"
 	if [ -d "$each" ]; then
-		echo $each
-		tar cf "$each.tar" "$each" && touch --reference="$each" "$each.tar" && rm -rf "$each"
-		ls -lart "$each.tar"
+		if [ ! -e "$tar" ]; then
+			echo $each
+			touch --reference="$each" "$temp_file"
+			tar cf "$tar" "$each" --remove-files
+			touch --reference="$temp_file" "$tar"
+			ls -lart "$tar"
+		else
+			echo "file \"${tar}\" already exists"
+		fi
 	fi
 done
+rm "$temp_file"
 
