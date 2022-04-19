d539f45092a16a2948c04b0d8e6884b401643cd7
mza <mza@phys.hawaii.edu>
2022-04-19 09:02:13 -1000

    bugfixes and improvements

--- setup/as_you_like_it.centos.sh
+++ setup/as_you_like_it.centos.sh
@@ -2,19 +2,55 @@
 
 # last updated 2022-04-19 by mza
 
-function update_repo_urls {
+declare interactive_ysq="ask"
+function interactive {
+	local command="${1}"
+	local description="${2}"
+	echo; echo "the command \"$command\" will:"; echo "$description"
+	if [ "$interactive_ysq" = "yes" ]; then
+		eval $command
+	else
+		echo "okay to continue?"
+		select s in yes skip-this-step quit; do
+			if [ "$s" = "yes" ]; then
+				echo "running \"$command\"..."
+				eval $command
+			elif [ "$s" = "skip-this-step" ]; then
+				echo "user skipped this step"
+				echo
+				return
+			elif [ "$s" = "quit" ]; then
+				echo "user quit $0"
+				if [ -z "$PS1" ]; then
+					exit 1
+				else
+					return
+				fi
+			fi
+			break;
+		done
+		echo
+	fi
+}
+
+function update_base_repo_urls {
 	# "rpm -qf /etc/issue" yields "centos-release-7-8.2003.0.el7.centos.x86_64"
-	#echo "  http://mirror.ancl.hawaii.edu/linux/epel/7/x86_64"
 	#echo " http://mirror.ancl.hawaii.edu/linux/centos/7/os/x86_64/"
-	interactive "sudo sed -i -e 's,#baseurl=.*basearch,baseurl=http://mirror.ancl.hawaii.edu/linux/epel/\$releasever/\$basearch,' -e 's,#baseurl=.*source,baseurl=http://mirror.ancl.hawaii.edu/linux/epel/\$releasever/source,' -e 's,^\(metalink=.*\),#\1,' /etc/yum.repos.d/epel.repo" "switch to use the local centos mirror"
-	interactive "sudo sed -i -e 's,^baseurl=.*/\([^/]\+\)/\$basearch,baseurl=http://mirror.ancl.hawaii.edu/linux/centos/\$releasever/\1/\$basearch,' /etc/yum.repos.d/CentOS-Base.repo" "switch to use the local epel mirror"
+	interactive "sudo sed -i -e 's,^[#]*baseurl=.*/\([^/]\+\)/\$basearch,baseurl=http://mirror.ancl.hawaii.edu/linux/centos/\$releasever/\1/\$basearch,' -e 's,^mirrorlist=,#mirrorlist=,' /etc/yum.repos.d/CentOS-Base.repo" "switch to use the local centos mirror"
+}
+
+function update_epel_repo_urls {
+	#echo "  http://mirror.ancl.hawaii.edu/linux/epel/7/x86_64"
+	interactive "sudo sed -i -e 's,#baseurl=.*basearch,baseurl=http://mirror.ancl.hawaii.edu/linux/epel/\$releasever/\$basearch,' -e 's,#baseurl=.*source,baseurl=http://mirror.ancl.hawaii.edu/linux/epel/\$releasever/source,' -e 's,^\(metalink=.*\),#\1,' /etc/yum.repos.d/epel.repo" "switch to use the local epel mirror"
 }
 
 function install_packages {
 	local list=""
 	#doublecmd-gtk
-	sudo yum -y --enablerepo=extras install epel-release
-	sudo yum -y update
+	update_base_repo_urls
+	nice sudo yum -y --enablerepo=extras install epel-release
+	update_epel_repo_urls
+	nice sudo yum -y update
 	list="$list gvim vim meld svn git gpm nmap ImageMagick firefox hdparm screen tmux rsync mlocate openssh-server net-tools gnuplot zip unzip dbus-x11 iptables-services"
 	list="$list yum-cron redhat-lsb gcc gcc-c++ make automake autoconf zip unzip ntp nfs-utils ntfs-3g"
 	list="$list dkms"
@@ -37,14 +73,13 @@ function install_packages {
 	#list="$list libtool clang texinfo libusb tcl-devel tcl pkgconfig libftdi # openocd"
 	#list="$list VirtualBox-6.0 # host"
 	#list="$list virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms # client"
-	sudo yum install -y $list
-	sudo yum -y groupinstall "Development Tools"
+	nice sudo yum install -y $list
+	nice sudo yum groupinstall -y "Development Tools"
 	echo "add the following line to /etc/ntp.conf:"
 	echo "	pool 192.168.153.1 iburst"
 	#echo "and to set the local timezone:"
 	#echo "	dpkg-reconfigure tzdata"
 }
 
-update_repo_urls
 install_packages
 
