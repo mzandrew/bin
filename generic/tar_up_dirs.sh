4d1a2aa6f6facaa8ded41081708c0df0fd110842
mza <mzandrew@gmail.com>
2022-04-21 12:40:44 -1000

    allow specifying what dirs to tar up on the command line

--- generic/tar_up_dirs.sh
+++ generic/tar_up_dirs.sh
@@ -2,28 +2,35 @@
 
 declare temp_file="$(mktemp /tmp/datestamp.XXXXXX)"
 
-for each in * .*; do
-	declare tar="${each}.tar"
-	declare lfr="${tar}.lf-r"
-	if [ "$each" == ".." ]; then continue; fi
-	if [ "$each" == "." ]; then continue; fi
-	#echo "$each"
-	if [ -d "$each" ]; then
-		if [ ! -e "$tar" ]; then
-			echo "$each"
-			(
-				cd "$each"
-				lf > "../${lfr}"
-			)
-			touch --reference="$each" "$temp_file"
-			tar cf "$tar" "$each" --remove-files
-			touch --reference="$temp_file" "$tar"
-			touch --reference="$temp_file" "$lfr"
-			ls -lart "$tar" "$lfr"
-		else
-			echo "file \"${tar}\" already exists"
+if [ $# -gt 0 ]; then
+	for each; do
+		declare tar="${each}.tar"
+		declare lfr="${tar}.lf-r"
+		if [ "$each" == ".." ]; then continue; fi
+		if [ "$each" == "." ]; then continue; fi
+		#echo "$each"
+		if [ -d "$each" ]; then
+			if [ ! -e "$tar" ]; then
+				echo "$each"
+				(
+					cd "$each"
+					lf > "../${lfr}"
+				)
+				touch --reference="$each" "$temp_file"
+				tar cf "$tar" "$each" --remove-files
+				touch --reference="$temp_file" "$tar"
+				touch --reference="$temp_file" "$lfr"
+				ls -lart "$tar" "$lfr"
+			else
+				echo "file \"${tar}\" already exists"
+			fi
 		fi
-	fi
-done
-rm "$temp_file"
+	done
+else
+	for each in * .*; do
+		$0 "${each}"
+	done
+fi
+
+rm -f "$temp_file"
 
