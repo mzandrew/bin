f001a13cc17fb311b147020152436d39397cd081
mza <mzandrew@gmail.com>
2022-04-23 17:24:10 -1000

    minor change

--- embedded/microsd_adafruit.py
+++ embedded/microsd_adafruit.py
@@ -1,4 +1,4 @@
-# last updated 2021-11-23 by mza
+# last updated 2022-04-23 by mza
 
 import adafruit_sdcard
 import storage
@@ -8,13 +8,13 @@ import digitalio
 from DebugInfoWarningError24 import debug, info, warning, error, debug2, debug3, set_verbosity, create_new_logfile_with_string_embedded, flush
 
 #spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-def setup_sdcard_for_logging_data(spi, cs_pin, dir):
+def setup_sdcard_for_logging_data(spi, cs_pin, dirname):
 	try:
 		#cs = digitalio.DigitalInOut(board.D6)
 		cs = digitalio.DigitalInOut(cs_pin)
 		sdcard = adafruit_sdcard.SDCard(spi, cs)
 		vfs = storage.VfsFat(sdcard)
-		storage.mount(vfs, dir) # this does NOT need an empty dir to mount on
+		storage.mount(vfs, dirname) # this does NOT need an empty dir to mount on
 		#with open("/logs/test.txt", "w") as f:
 		#	f.write("Hello, World!\r\n")
 	except:
