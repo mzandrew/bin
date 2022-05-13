9d3d63aa7754d0bcbd16188dfc3edbf1324a8b6c
mza <mzandrew@gmail.com>
2022-05-13 03:50:24 -1000

    boot.py now deletes self after running

--- embedded/custom_label.boot.py
+++ embedded/custom_label.boot.py
@@ -3,14 +3,20 @@
 # written 2021-04-21 by mza
 # last updated 2022-05-13 by mza
 
-import storage
-
 try:
+	import storage
 	storage.remount("/", readonly=False)
 	m = storage.getmount("/")
-	m.label = "CUSTOM"
-	storage.remount("/", readonly=True)
+	#m.label = "CUSTOM"
+	#m.label = "CONTINENTAL"
+	m.label = "ELDORADO"
+	try:
+		import os
+		os.unlink("boot.py")
+	except:
+		print("delete self was NOT successful")
 	storage.enable_usb_drive()
+	storage.remount("/", readonly=True)
 except:
 	print("rename was NOT successful")
 
