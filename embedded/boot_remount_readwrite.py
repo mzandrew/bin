41086d9ca568eea1a060cc45011aedb7f46102a5
mza <mzandrew@gmail.com>
2022-05-13 02:18:05 -1000

    minor change

--- embedded/boot_remount_readwrite.py
+++ embedded/boot_remount_readwrite.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 # written 2021-04-21 by mza
-# last updated 2021-04-21 by mza
+# last updated 2022-05-13 by mza
 
 import storage
+
 try:
-	storage.remount("/", False)
-	print("storage remount sucessful")
+	m = storage.remount("/", readonly=False)
+	print("storage remount (1) sucessful")
+	m.label = "custom"
+	try:
+		storage.remount("/", readonly=True)
+		print("storage remount (2) sucessful")
+	except:
+		print("storage remount (2) was NOT successful")
 except:
-	print("storage remount was NOT successful")
+	print("storage remount (1) was NOT successful")
 
