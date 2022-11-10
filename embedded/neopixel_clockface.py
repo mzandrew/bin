8d4754f2bda5720af8a9492f64598d91ead3fd85
mza <mzandrew@gmail.com>
2022-11-09 19:50:32 -1000

    add option for 24 led hour hand

--- embedded/neopixel_clockface.py
+++ embedded/neopixel_clockface.py
@@ -7,11 +7,16 @@
 import time
 import board
 import neopixel
-import digitalio
 
 #brightness = 0.04
 brightness = 1.0
-hours = neopixel.NeoPixel(board.A3, 12, pixel_order=(1, 0, 2, 3), brightness=brightness, auto_write=False)
+MAX_MINUTE = 59
+MAX_HOUR = 11
+if 1:
+	hours = neopixel.NeoPixel(board.A3, 12, pixel_order=(1, 0, 2, 3), brightness=brightness, auto_write=False)
+else:
+	hours = neopixel.NeoPixel(board.A3, 24, pixel_order=(1, 0, 2, 3), brightness=brightness, auto_write=False)
+	MAX_HOUR = 23
 minutes = neopixel.NeoPixel(board.D2, 60, pixel_order=(1, 0, 2, 3), brightness=brightness, auto_write=False)
 BLACK = (0, 0, 0, 0)
 PURPLE = (180, 0, 255, 0)
@@ -25,12 +30,12 @@ while True:
 	hours.fill(BLACK)
 	hours[h] = BLUE
 	h += 1
-	if 11<h:
+	if MAX_HOUR<h:
 		h = 0
 	minutes.fill(BLACK)
 	minutes[m] = BLUE
 	m += 1
-	if 59<m:
+	if MAX_MINUTE<m:
 		m = 0
 	hours.show()
 	minutes.show()
