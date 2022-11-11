75a438d633f5a22547f82509dbfac5cd7201449b
mza <mzandrew@gmail.com>
2022-11-11 09:38:45 -1000

    quantize in etas; fabs compare against eta; minor formatting changes

--- embedded/neopixel_clockface.py
+++ embedded/neopixel_clockface.py
@@ -2,8 +2,9 @@
 # with help from https://learn.adafruit.com/adafruit-circuit-playground-express/circuitpython-neopixel
 # and from https://learn.adafruit.com/adafruit-circuit-playground-express/circuitpython-digital-in-out
 # and from https://learn.adafruit.com/circuitpython-essentials/circuitpython-neopixel
-# last updated 2022-11-10 by mza
+# last updated 2022-11-11 by mza
 
+# to install:
 # cd lib
 # rsync -r adafruit_ds3231.mpy adafruit_as7341.mpy neopixel.mpy adafruit_minimqtt simpleio.mpy adafruit_esp32spi adafruit_register adafruit_io adafruit_requests.mpy adafruit_bus_device /media/mza/CIRCUITPY/lib/
 # cd ..
@@ -12,6 +13,7 @@
 
 import time
 import re
+import math
 import board
 import busio
 import neopixel
@@ -42,6 +44,7 @@ hour_color = BLUE
 
 N = 15 # average this many sensor readings before acting on it
 my_wifi_name = "neopixelclock"
+eta = 1.0/512.0
 
 if "adafruit_qtpy_esp32s2"==board.board_id:
 	hours_neopixel_pin = board.A0
@@ -49,6 +52,16 @@ if "adafruit_qtpy_esp32s2"==board.board_id:
 else: # kb2040
 	hours_neopixel_pin = board.A3
 	minutes_neopixel_pin = board.D2
+
+def quantize_in_etas(value):
+	#info(str(value))
+	value *= 1.0/eta
+	#info(str(value))
+	value = int(value)
+	#info(str(value))
+	value *= eta
+	#info(str(value))
+	return value
 
 def setup():
 	global i2c
@@ -79,6 +92,9 @@ def setup():
 		raise
 	except:
 		RTC_is_available = False
+	if RTC_is_available:
+		string = ds3231_adafruit.get_timestring2()
+		info(string)
 	setup_neopixel_clockface()
 	global airlift_is_available
 	airlift_is_available = airlift.setup_wifi(my_wifi_name)
@@ -91,7 +107,6 @@ def setup_neopixel_clockface():
 	global minutes
 	global brightness
 	brightness = NEOPIXEL_BRIGHTNESS_MIN
-	#brightness = NEOPIXEL_BRIGHTNESS_MAX
 	hours = neopixel.NeoPixel(hours_neopixel_pin, NUMBER_OF_HOUR_PIXELS, pixel_order=(1, 0, 2, 3), brightness=1.0, auto_write=False)
 	minutes = neopixel.NeoPixel(minutes_neopixel_pin, NUMBER_OF_MINUTE_PIXELS, pixel_order=(1, 0, 2, 3), brightness=1.0, auto_write=False)
 
@@ -113,15 +128,18 @@ def check_ambient_brightness():
 			brightness = NEOPIXEL_BRIGHTNESS_MAX
 		else:
 			brightness = NEOPIXEL_BRIGHTNESS_MIN + (NEOPIXEL_BRIGHTNESS_MAX-NEOPIXEL_BRIGHTNESS_MIN)*(values[AMBIENT_CHANNEL]-AMBIENT_BRIGHTNESS_FOR_MIN_NEOPIXEL_BRIGHTNESS)/(AMBIENT_BRIGHTNESS_FOR_MAX_NEOPIXEL_BRIGHTNESS-AMBIENT_BRIGHTNESS_FOR_MIN_NEOPIXEL_BRIGHTNESS)
-		info("ch[" + str(AMBIENT_CHANNEL) + "] = " + str(values[AMBIENT_CHANNEL]) + "; new brightness = " + str(brightness))
+		brightness = quantize_in_etas(brightness)
+		global brightness_string
+		brightness_string = "ch[" + str(AMBIENT_CHANNEL) + "] = " + str(values[AMBIENT_CHANNEL]) + "; new brightness = " + str(brightness)
 		n = 0
-		if not old_brightness==brightness:
+		if eta<math.fabs(old_brightness-brightness):
 			should_update_clockface = True
+			debug("updating clockface due to ambient light conditions change")
 		old_brightness = brightness
 	n += 1
 
 def draw_clockface():
-	#info("updating clockface...")
+	debug("updating clockface...")
 	hours.fill(list(map(lambda x: int(x*brightness), BLACK)))
 	for hh in range(0, NUMBER_OF_HOUR_PIXELS, NUMBER_OF_HOUR_PIXELS//4):
 		hours[hh] = list(map(lambda x: int(x*brightness), DOT_HOUR))
@@ -142,7 +160,7 @@ def parse_RTC():
 	global should_check_network_time
 	if RTC_is_available:
 		string = ds3231_adafruit.get_timestring2()
-		#info(string)
+		debug(string)
 		match = re.search("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\.([0-9][0-9])([0-9][0-9])([0-9][0-9])", string)
 		if match:
 			h = match.group(1)
@@ -152,16 +170,19 @@ def parse_RTC():
 			m = int(m)
 			s = int(s)
 			if not old_hour==h:
+				info(string + " " + brightness_string)
 				if 0==h:
 					should_check_network_time = True
-					info(string)
+					debug("just past midnight - need to update RTC from network time")
+			else:
+				debug(brightness_string)
 			old_hour = h
 			h = h % 12
 			if not old_minute==m:
 				should_update_clockface = True
-				#info("hour = " + str(h))
-				#info("minute = " + str(m))
-				#info("second = " + str(s))
+				debug("hour = " + str(h))
+				debug("minute = " + str(m))
+				debug("second = " + str(s))
 			old_minute = m
 
 def loop():
@@ -185,8 +206,10 @@ if __name__ == "__main__":
 	old_minute = 0
 	old_hour = 0
 	old_brightness = 0
+	brightness_string = ""
 	should_update_clockface = True
 	should_check_network_time = False
+	info("")
 	while True:
 		loop()
 
