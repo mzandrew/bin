a3a8a679fb424d148cb21fb6733f254c2bcb31c4
mza <mzandrew@gmail.com>
2022-12-01 17:44:19 -1000

    yellow sparkle mode

--- embedded/orange_flicker.py
+++ embedded/orange_flicker.py
@@ -2,7 +2,7 @@
 # with help from https://learn.adafruit.com/adafruit-circuit-playground-express/circuitpython-neopixel
 # and from https://learn.adafruit.com/adafruit-circuit-playground-express/circuitpython-digital-in-out
 # based on purple.py
-# last updated 2022-10-30 by mza
+# last updated 2022-12-01 by mza
 
 import time
 import board
@@ -10,11 +10,13 @@ import random
 import neopixel
 import digitalio
 
-#brightness = 0.1
+#brightness = 0.1 # dim
+#brightness = 0.25 # medium
 brightness = 1.0 # avert your eyes!
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=brightness, auto_write=True)
 BLACK = (0, 0, 0)
-red_to_green = 6
+#red_to_green = 6 # good for pumpkin orange
+red_to_green = 2 # good for yellow/golden sparkle
 green_max = 255 // red_to_green
 #value = green_max
 #ORANGE = (red_to_green*value, value, 0)
