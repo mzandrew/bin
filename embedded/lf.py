54433bbaa9799b80fcc88517a4641080864a38e5
mza <mzandrew@gmail.com>
2022-12-03 12:27:40 -1000

    to list files

---
+++ embedded/lf.py
@@ -0,0 +1,9 @@
+# to install:
+# cd lib
+# rsync -av adafruit_sdcard.mpy /media/mza/CIRCUITPY/lib/
+# cd ..
+# cp -a lf.py /media/mza/CIRCUITPY/code.py; sync
+
+import microsd_adafruit
+microsd_adafruit.list_files("")
+
