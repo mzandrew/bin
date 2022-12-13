1161321fb19cb08f47a1200c9cc26677e26dcbc0
mza <mzandrew@gmail.com>
2022-12-13 10:25:21 -1000

    squeezed code down a lot to get boxcar and ina260 functionality to fit in memory of ATSAMD21G18

--- embedded/loralight.py
+++ embedded/loralight.py
@@ -1,120 +1,115 @@
-# written 2022-12-07 by mza
-# based on lora.py and lora_transceiver_basic_test.py
-# last updated 2022-12-13 by mza
-
-import time
-import board
-import busio
-import digitalio
-import adafruit_rfm9x
-import gc
+# written 2022-12-07 to 2022-12-13 by mza
+import time, board, busio, digitalio, adafruit_rfm9x, gc
 
 PREFIX = "SCOOPY"
 SUFFIX = "BOOPS"
-node_type = "gathering"
-BAUD_RATE = 4*57600
-RADIO_FREQ_MHZ = 905.0 # (902-928 MHz in US/MEX/CAN)
-TX_POWER_DBM = 5 # [5, 23]
+BAUD = 4*57600
+RF = 905.0 # [902,928]
+TXDBM = 5 # [5, 23]
 N = 6
-ina260_N = 6
+ina_N = 4
 delay = 10
-ina260_bins = 2
-ina260_address = 0x40
-should_use_ina260 = True
+ina_bins = 2
+nodeid = 3
 
-def ina260_adafruit_get_values(bin=0):
-	values = [ ina260.current, ina260.voltage, ina260.power ]
-	#myboxcar.accumulate(values, bin)
-	return values
-
-def ina260_adafruit_show_average_values(bin=0):
-	#myboxcar.show_average_values(bin)
-	print("0, 0, 0")
-
-def ina260_adafruit_get_average_values(bin=0):
-	#return myboxcar.get_average_values(bin)
-	return [ 0, 0, 0 ]
-
-message_id = 1
-def send_a_message(message):
-	global message_id
-	message_id += 1
-	message_id_string = "[" + str(message_id) + "] "
-	message_with_prefix_and_suffix = PREFIX + message_id_string + message + SUFFIX
-	print("sending: " + message_id_string + message)
-	rfm9x.send(bytes(message_with_prefix_and_suffix, "utf-8"))
-
-def setup(spi, cs, reset, frequency, baudrate, tx_power_dbm):
+def bc_setup(numitems, numbins=1):
+	global items
+	items = numitems
+	global bins
+	bins = numbins
+	global acc_vals
+	acc_vals = [ [ [ 0. for a in range(items) ] for b in range(N) ] for c in range(bins) ]
+	global sums
+	sums = [ [ 0. for a in range(items) ] for b in range(bins) ]
+	global Nacc
+	Nacc = [ 0 for a in range(bins) ]
 	gc.collect()
-	print("mem free setup(): " + str(gc.mem_free()))
-	global rfm9x
-	rfm9x = adafruit_rfm9x.RFM9x(spi=spi, cs=cs, reset=reset, frequency=frequency, baudrate=baudrate)
-	rfm9x.tx_power = tx_power_dbm
+
+def bc_acc(vals, mybin=0):
+	global Nacc
+	Nacc[mybin] += 1
+	global acc_vals
+	acc_vals[mybin].append(vals[:])
+	for i in range(items):
+		sums[mybin][i] = 0.
+		for j in range(1, len(acc_vals[mybin])):
+			sums[mybin][i] += acc_vals[mybin][j][i]
+	acc_vals[mybin].pop(0)
+	gc.collect()
+
+def bc_get_avg_vals(mybin=0):
+	if Nacc[mybin]<N:
+		n = Nacc[mybin]
+	else:
+		n = N
+	if 0==n:
+		return
+	avg_vals = [ sums[mybin][i]/float(n) for i in range(items) ]
+	return avg_vals
+
+def ina_get_vals(mybin=0):
+	vals = [ ina.current, ina.voltage, ina.power ]
+	bc_acc(vals, mybin)
+	return vals
+
+def ina_get_avg_vals(mybin=0):
+	return bc_get_avg_vals(mybin)
+
+def ina_show_avg_vals(mybin=0):
+	print(str(ina_get_avg_vals(mybin)))
+
+msgid = 0
+def send(msg):
+	global msgid
+	msgid += 1
+	msgid_str = "node" + str(nodeid) + "[" + str(msgid) + "] "
+	print(msgid_str + msg)
+	rfm9x.send(bytes(PREFIX + msgid_str + msg + SUFFIX, "utf-8"))
 
 def loop():
-	gc.collect()
-	print("mem free loop(): " + str(gc.mem_free()))
 	i = 0
 	while True:
-		if ina260_is_available:
-			ina260_adafruit_get_values(0)
+		if ina_exists:
+			ina_get_vals(0)
 		if 0==i%N:
-			print("doing something")
-			if ina260_is_available:
-				ina260_adafruit_show_average_values(0)
-				values = ina260_adafruit_get_average_values(0)
-				string = str(values)
-				#send_a_message("ina260bin0 " + string)
-				ina260_adafruit_get_values(1)
-#				ina260_adafruit_show_average_values(1)
-#				values = ina260_adafruit_get_average_values(1)
-#				string = str(values)
-#				send_a_message("ina260bin1 " + string)
+			print("yup")
+			if ina_exists:
+				ina_show_avg_vals(0)
+				send("ina260bin0 " + str(ina_get_avg_vals(0)))
+				time.sleep(2)
+				ina_get_vals(1)
+				ina_show_avg_vals(1)
+				send("ina260bin1 " + str(ina_get_avg_vals(1)))
+				ina_get_vals(1)
 		else:
-			print("did nothing")
+			print("nope")
 		i += 1
 		time.sleep(delay)
 
 if __name__ == "__main__":
-	spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-	CS = digitalio.DigitalInOut(board.RFM9X_CS)
-	RESET = digitalio.DigitalInOut(board.RFM9X_RST)
+	rfm9x = adafruit_rfm9x.RFM9x(spi=busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO), cs=digitalio.DigitalInOut(board.RFM9X_CS), reset=digitalio.DigitalInOut(board.RFM9X_RST), frequency=RF, baudrate=BAUD)
+	rfm9x.tx_power = TXDBM
 	gc.collect()
-	setup(spi, CS, RESET, RADIO_FREQ_MHZ, BAUD_RATE, TX_POWER_DBM)
 	i2c = busio.I2C(board.SCL, board.SDA)
-	global ina260_is_available
-	ina260_is_available = False
-	gc.collect()
-	print("mem free ina260: " + str(gc.mem_free()))
-	if should_use_ina260:
-		try:
-			import adafruit_ina260
-			global ina260
-			ina260 = adafruit_ina260.INA260(i2c_bus=i2c, address=ina260_address)
-			adafruit_ina260.INA260.mode = adafruit_ina260.Mode.TRIGGERED
-			adafruit_ina260.INA260.current_conversion_time = adafruit_ina260.ConversionTime.TIME_8_244_ms
-			adafruit_ina260.INA260.voltage_conversion_time = adafruit_ina260.ConversionTime.TIME_8_244_ms
-			adafruit_ina260.INA260.averaging_count = adafruit_ina260.AveragingCount.COUNT_1024
-		except:
-			print("warning: can't talk to ina260 at address " + str(ina260_address))
-		try:
-			global myboxcar
-			gc.collect()
-			print("mem free boxcar: " + str(gc.mem_free()))
-			import boxcar
-			gc.collect()
-			print("mem free boxcar: " + str(gc.mem_free()))
-			gc.collect()
-			print("mem free boxcar: " + str(gc.mem_free()))
-			myboxcar = boxcar.boxcar(3, ina260_N, "ina260", ina260_bins)
-			gc.collect()
-			print("mem free boxcar: " + str(gc.mem_free()))
-			ina260_is_available = True
-		except MemoryError as error_message:
-			print("MemoryError: " + str(error_message))
-		except:
-			print("warning: can't setup boxcars")
-#	if ina260_is_available:
-#		ina260_adafruit.get_values(1)
+	ina_exists = False
+	try:
+		import adafruit_ina260
+		gc.collect()
+		global ina
+		ina = adafruit_ina260.INA260(i2c_bus=i2c, address=0x40)
+		gc.collect()
+		ina_exists = True
+	except:
+		print("can't setup ina260")
+		raise
+	try:
+		gc.collect()
+		bc_setup(3, ina_bins)
+		gc.collect()
+	except:
+		print("can't setup boxcar")
+		raise
+	if ina_exists:
+		ina_get_vals(1)
 	loop()
 
