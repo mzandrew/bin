94aa0c279720fe2c582aed43a84863ab7fe57e91
mza <mzandrew@gmail.com>
2023-01-06 15:56:36 -1000

    draws plot captions

--- embedded/mondrian.py
+++ embedded/mondrian.py
@@ -6,10 +6,15 @@
 
 SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 720
-GAP_X_BETWEEN_PLOTS = 10
-GAP_Y_BETWEEN_PLOTS = 10
+GAP_X_BETWEEN_PLOTS = 14
+GAP_Y_BETWEEN_PLOTS = 44
 GAP_X_SIDE = 10
-GAP_Y_TOP_BOTTOM = 10
+GAP_Y_TOP = 24
+GAP_Y_BOTTOM = 24
+FONT_SIZE = 18
+ICON_SIZE = 32
+ICON_BORDER = 2
+ICON_SQUARE_LENGTH = ICON_SIZE//2 - 3*ICON_BORDER
 
 ROWS = 2
 COLUMNS = 2
@@ -18,22 +23,22 @@ feed_name = [ [ [] for j in range(ROWS) ] for i in range(COLUMNS) ]
 minimum = [ [ 0 for j in range(ROWS) ] for i in range(COLUMNS) ]
 maximum = [ [ 100 for j in range(ROWS) ] for i in range(COLUMNS) ]
 
-plot_name[0][0] = "temp"
+plot_name[0][0] = "temperature"
 minimum[0][0] = 10.
 maximum[0][0] = 80.
 feed_name[0][0] = [ "roof-temp", "outdoor-temp", "inside-temp", "heater" ]
 
-plot_name[1][0] = "hum"
+plot_name[1][0] = "humidity"
 minimum[1][0] = 40.
 maximum[1][0] = 100.
 feed_name[1][0] = [ "roof-hum", "outdoor-hum", "inside-hum", "indoor2-hum" ]
 
-plot_name[0][1] = "pres"
+plot_name[0][1] = "pressure"
 minimum[0][1] = 0.997
 maximum[0][1] = 1.008
 feed_name[0][1] = [ "pressure", "indoor2-pressure" ]
 
-plot_name[1][1] = "particle"
+plot_name[1][1] = "particle count"
 minimum[1][1] = 0.
 maximum[1][1] = 350.
 feed_name[1][1] = [ "indoor-0p3", "indoor-0p5", "indoor-1p0", "indoor-2p5", "indoor-5p0", "particle0p3", "particle0p5", "particle1p0", "particle2p5", "particle5p0", "particle10p0" ]
@@ -65,6 +70,8 @@ import pygame # sudo apt install -y python3-pygame
 from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT, K_q, K_BREAK, K_SPACE
 import fetch
 
+FAKE_DATA = True
+
 def clear_plot(i, j):
 	plot[i][j].fill(black)
 
@@ -86,6 +93,8 @@ def clear_plots():
 #DEFAULT_VALUE = -40
 DEFAULT_VALUE = 35.
 def pad_data_if_insufficient(values, count_desired):
+	if values is None:
+		values = []
 	count_gotten = len(values)
 	if count_desired is not None:
 		if count_desired<count_gotten:
@@ -106,15 +115,14 @@ def fetch_data_for_the_first_time(i, j):
 	print(plot_name[i][j])
 	for k in range(len(feed_name[i][j])):
 		print("fetching data for feed \"" + feed_name[i][j][k] + "\"...")
-		#if 0==i and 0==j and 0==k:
-		#	pass
+		if not FAKE_DATA:
 			#feed_data[i][j][k] = fetch.fetch_list_with_datestamps(feed_name[i][j][k], count=plot_width)
-		feed_data[i][j][k] = fetch.fetch_simple_list(feed_name[i][j][k], count=plot_width)
-		print("length of returned data = " + str(len(feed_data[i][j][k])))
+			feed_data[i][j][k] = fetch.fetch_simple_list(feed_name[i][j][k], count=plot_width)
+			print("length of returned data = " + str(len(feed_data[i][j][k])))
+			time.sleep(0.350)
 		#feed_data[i][j][k] = [ DEFAULT_VALUE for a in range(plot_width//2) ]
 		feed_data[i][j][k] = pad_data_if_insufficient(feed_data[i][j][k], plot_width)
 		#print("length of data = " + str(len(feed_data[i][j][k])))
-		time.sleep(0.350)
 
 def update_plots():
 	#clear_plots()
@@ -124,9 +132,10 @@ def update_plots():
 
 def update_plot(i, j):
 	for k in range(len(feed_name[i][j])):
-		#print("fetching another datapoint for feed \"" + feed_name[i][j][k] + "\"...")
-		feed_data[i][j][k] = fetch.add_most_recent_data_to_end_of_array(feed_data[i][j][k], feed_name[i][j][k])
-		#print("length of data = " + str(len(feed_data[i][j][k])))
+		if not FAKE_DATA:
+			#print("fetching another datapoint for feed \"" + feed_name[i][j][k] + "\"...")
+			feed_data[i][j][k] = fetch.add_most_recent_data_to_end_of_array(feed_data[i][j][k], feed_name[i][j][k])
+			#print("length of data = " + str(len(feed_data[i][j][k])))
 	global plots_were_updated
 	print("normalizing data...")
 	for k in range(len(feed_name[i][j])):
@@ -153,7 +162,7 @@ def update_plot(i, j):
 
 def draw_plot_border(i, j):
 	print("drawing plot border...")
-	pygame.draw.rect(screen, white, pygame.Rect(GAP_X_SIDE+i*(plot_width+GAP_X_BETWEEN_PLOTS)-1, GAP_Y_TOP_BOTTOM+j*(plot_height+GAP_Y_BETWEEN_PLOTS)-1, plot_width+2, plot_height+2), 1)
+	pygame.draw.rect(screen, white, pygame.Rect(GAP_X_SIDE+i*(plot_width+GAP_X_BETWEEN_PLOTS)-1, GAP_Y_TOP+j*(plot_height+GAP_Y_BETWEEN_PLOTS)-1, plot_width+2, plot_height+2), 1)
 
 def setup():
 	fetch.setup()
@@ -163,7 +172,7 @@ def setup():
 	global plot
 	usable_width = SCREEN_WIDTH - 2*GAP_X_SIDE - (COLUMNS-1)*GAP_X_BETWEEN_PLOTS
 	#print("usable_width: " + str(usable_width))
-	usable_height = SCREEN_HEIGHT - 2*GAP_Y_TOP_BOTTOM - (ROWS-1)*GAP_Y_BETWEEN_PLOTS
+	usable_height = SCREEN_HEIGHT - GAP_Y_TOP - GAP_Y_BOTTOM - (ROWS-1)*GAP_Y_BETWEEN_PLOTS
 	#print("usable_height: " + str(usable_height))
 	plot_width = int(usable_width / COLUMNS)
 	plot_height = int(usable_height / ROWS)
@@ -181,15 +190,26 @@ def setup():
 	pygame.init()
 	pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
 	pygame.display.set_caption("mondrian")
-	#size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-	size = (SCREEN_WIDTH, SCREEN_HEIGHT)
-	screen = pygame.display.set_mode(size)
+	myfont = pygame.font.SysFont("monospace", FONT_SIZE)
+#	icon = pygame.Surface((ICON_SIZE, ICON_SIZE))
+#	for i in range(COLUMNS):
+#		for j in range(ROWS):
+#			pass
+#			#pygame.draw.rect(icon, (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)), pygame.Rect(ICON_BORDER+i*(ICON_SQUARE_LENGTH+ICON_BORDER), ICON_BORDER+j*(ICON_SQUARE_LENGTH+ICON_BORDER), ICON_SQUARE_LENGTH, ICON_SQUARE_LENGTH))
+#	icon.fill(yellow)
+#	pygame.display.set_icon(icon)
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	screen.fill(black)
 	plot = [ [ pygame.Surface((plot_width, plot_height)) for j in range(ROWS) ] for i in range(COLUMNS) ]
 	#plot_rect = [ [ plot[i][j].get_rect() for j in range(ROWS) ] for i in range(COLUMNS) ]
 	#clear_plots()
 	for i in range(COLUMNS):
 		for j in range(ROWS):
+			pygame.event.pump()
+			plot_caption = myfont.render(plot_name[i][j], 1, white)
+			screen.blit(plot_caption, plot_caption.get_rect(center=(GAP_X_SIDE+i*(plot_width+GAP_X_BETWEEN_PLOTS)+plot_width//2 , GAP_Y_TOP+j*(plot_height+GAP_Y_BETWEEN_PLOTS)-FONT_SIZE//2-4)))
+#			for k in range(len(feed_name[i][j])):
+#				feed_caption = myfont.render(feed_name[i][j], 1, color[k+2])
 			fetch_data_for_the_first_time(i, j)
 			draw_plot_border(i, j)
 			update_plot(i, j)
@@ -242,7 +262,7 @@ def blit(i, j):
 	global something_was_updated
 	if plots_were_updated[i][j]:
 		print("blitting...")
-		screen.blit(plot[i][j], (GAP_X_SIDE+i*(plot_width+GAP_X_BETWEEN_PLOTS), GAP_Y_TOP_BOTTOM+j*(plot_height+GAP_Y_BETWEEN_PLOTS)))
+		screen.blit(plot[i][j], (GAP_X_SIDE+i*(plot_width+GAP_X_BETWEEN_PLOTS), GAP_Y_TOP+j*(plot_height+GAP_Y_BETWEEN_PLOTS)))
 		plots_were_updated[i][j] = False
 		something_was_updated = True
 
