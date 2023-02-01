80a67a173f428e37daffc08c44483a18823ea9a7
mza <mza@phys.hawaii.edu>
2023-02-01 10:57:32 -1000

    installs opencv

---
+++ setup/as_you_like_it.opencv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# following https://raspberrypi-guide.github.io/programming/install-opencv
+
+#sudo apt update
+#sudo apt install -y build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-103 python3-pyqt5 python3-dev python3-pip python3-wheel
+#pip3 install --upgrade pip wheel setuptools numpy imutils
+#pip3 install --upgrade numpy imutils setuptools
+pip3 install opencv-python
+
