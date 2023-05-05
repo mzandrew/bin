ce22d20132d76489ae077f6afee19c02310dfd77
mza <mzandrew@gmail.com>
2023-05-05 04:35:12 -1000

    can run on a list of files now

--- generic/selpy-fix.sh
+++ generic/selpy-fix.sh
@@ -1,25 +1,34 @@
 #!/bin/bash -e
 
 # written 2020-06 by mza
-# last updated 2020-08-26 by mza
+# last updated 2023-05-05 by mza
+
+function run_file {
+	local each="${@}"
+	if [ -e "${each}" ]; then
+		echo "$each"
+		convert -interlace none $each $each
+		convert $each -thumbnail '196x196>' ${each}.thumb
+		exiftool "-ThumbnailImage<=${each}.thumb" -P -overwrite_original $each
+		rm ${each}.thumb
+	fi
+}
 
 # https://superuser.com/a/598696/539022
 function run {
-	local dir="${@}"
-	if [ -e "${dir}" ] && [ -d "${dir}" ]; then
-		find "${dir}" -maxdepth 1 -type f -iname "*.jpg" -o -iname "*.jpeg" | while read each; do
-			echo "$each"
-			convert -interlace none $each $each
-			convert $each -thumbnail '196x196>' ${each}.thumb
-			exiftool "-ThumbnailImage<=${each}.thumb" -P -overwrite_original $each
-			rm ${each}.thumb
-			# fix datestamp with exif data
-			# ...
-			# must be named .jpg, not .jpeg:
-#			if [ "${each: -4}" == "jpeg" ]; then
-#				mv "$each" "$new"
-#			fi
+	local item="${@}"
+	if [ -d "${item}" ]; then
+		find "${item}" -maxdepth 1 -type f -iname "*.jpg" -o -iname "*.jpeg" | while read each; do
+			run_file "$each"
+		# fix datestamp with exif data
+		# ...
+		# must be named .jpg, not .jpeg:
+#		if [ "${each: -4}" == "jpeg" ]; then
+#			mv "$each" "$new"
+#		fi
 		done
+	else
+		run_file "$each"
 	fi
 }
 
