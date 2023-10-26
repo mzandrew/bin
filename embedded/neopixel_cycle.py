f4808c6bc22faafbba68d938bac1ecb102659465
mzandrew <mzandrew@gmail.com>
2023-10-26 13:28:41 -1000

    adds better/smoother rainbow cycling code

--- embedded/neopixel_cycle.py
+++ embedded/neopixel_cycle.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
 
 # written 2021-04-21 by mza
-# last updated 2021-04-21 by mza
+# with help from https://learn.adafruit.com/adafruit-neopixel-driver-bff/circuitpython
+# last updated 2023-10-26 by mza
+
+# to install:
+# cp -ar adafruit_pixelbuf.mpy neopixel.mpy /media/mza/CIRCUITPY/lib/
+# cp -a neopixel_cycle.py /media/mza/NEO90/code.py; sync
 
 import time
 import board
+from rainbowio import colorwheel
 import neopixel
 
-neopin = board.NEOPIXEL
-pixel = neopixel.NeoPixel(neopin, 1, brightness=0.2, auto_write=False)
+if 0:
+	NUMPIXELS = 1
+	PIN = board.NEOPIXEL
+else:
+	NUMPIXELS = 90
+	PIN = board.A3
+
+SPEED = 0.001
+BRIGHTNESS = 0.2
+pixels = neopixel.NeoPixel(PIN, NUMPIXELS, brightness=BRIGHTNESS, auto_write=False)
+
+def rainbow_cycle(wait):
+	for color in range(255):
+		for pixel in range(NUMPIXELS):
+			pixel_index = (pixel * 256 // NUMPIXELS) + color * 5
+			pixels[pixel] = colorwheel(pixel_index & 255)
+		pixels.show()
+		time.sleep(wait)
+
 while True:
-	pixel.fill((255, 255, 0))
-	pixel.show()
-	time.sleep(1.0)
-	pixel.fill((255, 0, 255))
-	pixel.show()
-	time.sleep(1.0)
-	pixel.fill((0, 255, 255))
-	pixel.show()
-	time.sleep(1.0)
+    rainbow_cycle(SPEED)
 
