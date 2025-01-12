5d5dc8e754c7ab5a4b5ce58fa103b9b3694da5b4
mzandrew <mzandrew@gmail.com>
2025-01-12 02:29:30 -1000

    adds ability to tar dirs up to a location other than just above where the dir exists

--- generic/tar_up_dirs.sh
+++ generic/tar_up_dirs.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
+# last updated 2025-01-12 by mza
+
 declare temp_file="$(mktemp /tmp/datestamp.XXXXXX)"
+declare local_dir=$(readlink -f "$(pwd)")
+declare -i verbosity=3
+if [ $verbosity -gt 3 ]; then echo "local_dir $local_dir"; fi
 
 if [ $# -gt 0 ]; then
 	for each; do
-		each=$(echo $each | sed -e "s,/$,,g")
-		declare tar="${each}.tar"
-		declare lfr="${tar}.lf-r"
-		if [ "$each" == ".." ]; then continue; fi
-		if [ "$each" == "." ]; then continue; fi
-		#echo "$each"
-		if [ -d "$each" ]; then
-			if [ ! -e "$tar" ]; then
-				echo "$each"
-				(
-					cd "$each"
-					lf > "../${lfr}"
-				)
-				touch --reference="$each" "$temp_file"
-				tar cf "$tar" "$each" --remove-files
-				touch --reference="$temp_file" "$tar"
-				touch --reference="$temp_file" "$lfr"
-				ls -lart "$tar" "$lfr"
-			else
-				echo "file \"${tar}\" already exists"
+		each=$(echo "$each" | sed -e "s,/$,,g")
+		if [ -e "$each" ]; then
+			basename=$(basename "$each")
+			if [ "$basename" == ".." ]; then continue; fi
+			if [ "$basename" == "." ]; then continue; fi
+			if [ $verbosity -gt 3 ]; then echo "basename $basename"; fi
+			declare tar="${local_dir}/${basename}.tar"
+			declare lfr="${local_dir}/${basename}.tar.lf-r"
+			if [ $verbosity -gt 3 ]; then echo "tar $tar"; fi
+			if [ $verbosity -gt 3 ]; then echo "lfr $lfr"; fi
+			parent_dir=$(dirname "$each")
+			parent_dir=$(readlink -f "$parent_dir")
+			if [ $verbosity -gt 3 ]; then echo "parent_dir $parent_dir"; fi
+			if [ -d "$parent_dir" ]; then
+				cd "$parent_dir"
+				if [ -d "$basename" ]; then
+					if [ ! -e "$tar" ]; then
+						echo "$basename"
+						(
+							cd "$basename"
+							lf > "${lfr}"
+						)
+						touch --reference="$basename" "$temp_file"
+						tar cf "$tar" "$basename" --remove-files
+						#tar cf "$tar" "$basename"
+						touch --reference="$temp_file" "$tar"
+						touch --reference="$temp_file" "$lfr"
+						ls -lart "$tar" "$lfr"
+					else
+						echo "file \"${tar}\" already exists"
+					fi
+				fi
 			fi
 		fi
 	done
