9b242863bd2fe65a5c37ff05912e8b00f2bd95a0
mza <mza@phys.hawaii.edu>
2025-05-09 14:44:17 -1000

    adds docker install script

---
+++ setup/as_you_like_it.docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+sudo apt install -y ca-certificates curl
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker pull ubuntu
+
