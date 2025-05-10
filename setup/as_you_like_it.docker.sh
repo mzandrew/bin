ed9f85b10d68aee73a3242f88040bbc5d3e2a577
mza <mza@phys.hawaii.edu>
2025-05-09 14:48:57 -1000

    minor change

--- setup/as_you_like_it.docker.sh
+++ setup/as_you_like_it.docker.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y ca-certificates curl
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable" | sudo tee -a /etc/apt/sources.list
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 docker pull ubuntu
